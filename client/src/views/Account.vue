9f78ef95ffea77b86bdc635927332a53b76b3bd4
SebastiaanvanVliet <690592@student.inholland.nl>
2023-03-09 13:03:11 +0100

    ...

--- client/src/views/Account.vue
+++ client/src/views/Account.vue
@@ -43,6 +43,20 @@ function handleLogout() {
     quizStore.resetQuiz();
     router.push('/');
 }
+
+async function handleQuizDelete(quizId: number) {
+    const respone = await axios.delete('/api/quizzes', {
+        params: {
+            id: quizId,
+        },
+        headers: {
+            Authorization: `Bearer ${loginStore.getToken}`,
+        },
+    });
+
+    // remove the quiz from the quizzes array
+    quizzes.value = quizzes.value.filter((quiz) => quiz.id !== quizId);
+}
 </script>
 
 <template>
@@ -61,10 +75,28 @@ function handleLogout() {
                     <h5 class="card-title">{{ quiz.name }}</h5>
                     <p class="card-text">Vragen: {{ quiz.questions.length }}</p>
                     <p class="cart-text">Opebaar: {{ quiz.public }}</p>
-                    <router-link :to="{ name: 'play', params: { id: quiz.id } }" class="btn btn-primary mx-1 mb-2">Speel
+                    <router-link :to="{ name: 'play', params: { id: quiz.id } }" class="button mx-1 mb-2">Speel
                         quiz</router-link>
+
+                    <!-- delete button -->
+                    <a @click="handleQuizDelete(quiz.id)">
+                        <svg width="45px" height="45px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path d="M4 7H20" stroke="#c2001d" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path
+                                    d="M6 10L7.70141 19.3578C7.87432 20.3088 8.70258 21 9.66915 21H14.3308C15.2974 21 16.1257 20.3087 16.2986 19.3578L18 10"
+                                    stroke="#c2001d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#c2001d"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </g>
+                        </svg>
+                    </a>
+
                     <!-- <a href="#" class="btn btn-primary mx-1 mb-2">Pas deze quiz aan</a>
-                                                                                                                                            <a href="#" class="btn btn-danger mx-1 mb-2">Verwijder deze quiz</a> -->
+                                                                                                                                                                                                                                            <a href="#" class="btn btn-danger mx-1 mb-2">Verwijder deze quiz</a> -->
                 </div>
             </div>
         </div>
@@ -75,6 +107,15 @@ function handleLogout() {
 a:hover {
     /* cursor */
     cursor: pointer;
+}
+
+.button {
+    font-family: 'Boogaloo', cursive;
+    color: white;
+    font-size: 1.5rem;
+    padding: 0.5rem 1rem;
+    border-radius: 2rem;
+    background-color: #0D5D56;
 }
 
 .top-right {
